beb351f95f3253180a644f0fd44d75b954ca2a56
Michelle Luo <luo.mich16@gmail.com>
2017-10-22 13:18:08 -0400

    Fix some formatting

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ import time
 import sys
 from keys import key
 
+USAGE = 'usage: python3 test.py <test-data>'
+
 def correct_text(original, corrections):
     """
-    given a json of corrections and the original line, returns
-    a string of the corrected version of original
+    given a json of corrections and the original line, returns a string
+    of the corrected version of original
     """
     if len(corrections['flaggedTokens']) == 0:
-        print('no suggestions')
         return original.replace('\n', '')
     misspelled = corrections['flaggedTokens']
     text = original.replace('\n', '')
@@ -24,7 +25,7 @@ def correct_text(original, corrections):
 
 def spell_check(text):
     """
-    send a string of text to bing for correction
+    send a string of text to bing for correction & return corrected version
     """
     if len(text) < 3:
         return
@@ -39,28 +40,29 @@ def spell_check(text):
 
 def misspell_text(line):
     """ 
-    systematically misspell each word in a line: 
-    first try: removing the second letter from each word    
+    systematically misspell each word in a line
+    * first try: removing the second letter from each word
     """
     line = line.split(' ')
     for i in range(len(line)):
-        if len(line[i]) > 2:
-            line[i] = line[i].replace(line[i][1], '')
+        if len(line[i]) > 3:
+            line[i] = line[i].replace(line[i][1], '', 1)
     return ' '.join(line)
 
 def main():
     if len(sys.argv) != 2:
-        print("usage: python3 test.py <test-data>")
+        print(USAGE)
         return
     fname = sys.argv[1]
     try: 
         with open(fname) as fileobj:
-            print('correcting', fname)
             for line in fileobj:
                 # one request to msft per second :(
                 time.sleep(1.25)
+                line = line.lstrip().rstrip()
                 line = misspell_text(line)
-                print(spell_check(line))
+                line = spell_check(line)
+                print(line)
     except FileNotFoundError:
         print('file not found')
 
