a67256fbf9e1e937f7d9ce6792b5a28a851e0a06
Michelle Luo <luo.mich16@gmail.com>
2017-10-22 11:18:58 -0400

    Remove second letter from each word

---
+++ README.md
@@ -0,0 +1,2 @@
+* 10.22: systematically removing letters from individual words
+  - yield: 
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import requests
-import keys
 import time
-
-def misspell_text(original, misspelled):
-    pass
+import sys
+from keys import key
 
 def correct_text(original, corrections):
+    """
+    given a json of corrections and the original line, returns
+    a string of the corrected version of original
+    """
     if len(corrections['flaggedTokens']) == 0:
+        print('no suggestions')
         return original.replace('\n', '')
     misspelled = corrections['flaggedTokens']
     text = original.replace('\n', '')
@@ -19,23 +22,45 @@ def correct_text(original, corrections):
         text[word_to_replace_i] = suggestion
     return ' '.join(text)
 
+def spell_check(text):
+    """
+    send a string of text to bing for correction
+    """
+    if len(text) < 3:
+        return
+    url = 'https://api.cognitive.microsoft.com/bing/v7.0/spellcheck?text=' + text
+    headers = {'Ocp-Apim-Subscription-Key': key, 'setLang': 'EN' }
+    r = requests.get(url, headers=headers)
+    if r.status_code == requests.codes.ok:
+        results = correct_text(text, r.json())
+        return results
+    else:
+        return r.text
+
+def misspell_text(line):
+    """ 
+    systematically misspell each word in a line: 
+    first try: removing the second letter from each word    
+    """
+    line = line.split(' ')
+    for i in range(len(line)):
+        if len(line[i]) > 2:
+            line[i] = line[i].replace(line[i][1], '')
+    return ' '.join(line)
+
 def main():
-    fname = input('provide file for testing data\n')
+    if len(sys.argv) != 2:
+        print("usage: python3 test.py <test-data>")
+        return
+    fname = sys.argv[1]
     try: 
         with open(fname) as fileobj:
             print('correcting', fname)
             for line in fileobj:
-                time.sleep(1)
-                if len(line) < 3:
-                    continue
-                url = 'https://api.cognitive.microsoft.com/bing/v7.0/spellcheck?text=' + line
-                headers = {'Ocp-Apim-Subscription-Key': keys.key1, 'setLang': 'EN' }
-                r = requests.get(url, headers=headers)
-                if r.status_code == requests.codes.ok:
-                    results = correct_text(line, r.json())
-                    print(results)
-                else:
-                    print(r.text)
+                # one request to msft per second :(
+                time.sleep(1.25)
+                line = misspell_text(line)
+                print(spell_check(line))
     except FileNotFoundError:
         print('file not found')
 
